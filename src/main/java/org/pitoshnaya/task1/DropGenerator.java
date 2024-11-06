ee73df314878c952b01d3ebff4a11836ad71e48d
Diba <diba_2012@mail.ru>
2024-11-06 16:29:27 +0300

    сгенерировал случайный дроп эдемента из List

--- src/main/java/org/pitoshnaya/task1/DropGenerator.java
+++ src/main/java/org/pitoshnaya/task1/DropGenerator.java
@@ -1,5 +1,24 @@
 package org.pitoshnaya.task1;
 
-public class DropGenerator {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
+public class DropGenerator {
+    public static void main(String[] args) {
+
+    }
+    public String getRandomItem() {
+        Random generator = new Random();
+        List<String> items = new ArrayList<>();
+        items.add("Item1");
+        items.add("Item2");
+        items.add("Item3");
+        items.add("Item4");
+        items.add("Item5");
+        var randomItem = items.get(generator.nextInt(items.size()));
+        System.out.println(randomItem);
+
+        return randomItem;
+    }
 }
